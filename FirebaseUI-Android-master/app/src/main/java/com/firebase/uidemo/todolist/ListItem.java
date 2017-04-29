adcef24086ccbd3d30a5c82eab736afa8f854e44
Jasmine Lu (jll68) <jll68@duke.edu>
2017-04-29 18:51:08 -0400

    indication if shared list

---
+++ FirebaseUI-Android-master/app/src/main/java/com/firebase/uidemo/todolist/ListItem.java
@@ -0,0 +1,42 @@
+package com.firebase.uidemo.todolist;
+
+import java.util.ArrayList;
+
+/**
+ * Created by JasmineLu on 4/29/17.
+ */
+
+public class ListItem {
+    private String listTitle;
+    private ArrayList<String> users;
+    private String myID;
+
+    public ListItem(String title, ArrayList<String> team, String id){
+        this.listTitle = (title == null) ? "": title;
+        this.users = (team == null) ? new ArrayList<String>() : team;
+        this.myID = (id == null) ? "": id;
+    }
+
+    public boolean isShared(){
+        if(users.size() > 1)
+            return true;
+        else
+            return false;
+    }
+
+    public String getListTitle(){
+        return listTitle;
+    }
+
+    public String getUserString(){
+        String userString = "";
+        for(String user: users)
+            userString = userString + " " + user + ",";
+        userString = userString.substring(0, userString.length() - 1);
+        return userString;
+    }
+
+    public String getMyID(){
+        return myID;
+    }
+}
