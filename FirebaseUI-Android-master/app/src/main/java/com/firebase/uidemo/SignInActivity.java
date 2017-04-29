8be7b389fe7392a2a60ef8a80e9aa3e5a82f6fed
jmb145 <jmb145@duke.edu>
2017-04-29 19:56:30 -0400

    forgot to add the files that I created

---
+++ FirebaseUI-Android-master/app/src/main/java/com/firebase/uidemo/SignInActivity.java
@@ -0,0 +1,156 @@
+package com.firebase.uidemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.MainThread;
+import android.support.annotation.StyleRes;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+import com.firebase.uidemo.auth.SignedInActivity;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by JordanBurton on 4/28/17.
+ */
+
+public class SignInActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 100;
+
+    private static final String UNCHANGED_CONFIG_VALUE = "CHANGE-ME";
+    private static final String GOOGLE_TOS_URL = "https://www.google.com/policies/terms/";
+    private static final String FIREBASE_TOS_URL = "https://firebase.google.com/terms/";
+    private static final String TAG = "SignInActivity";
+
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser() != null){
+            Log.d(TAG, "Going to ChooserActivity");
+            startActivity(new Intent(this, ChooserActivity.class));
+            finish();
+            return;
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            handleSignInResponse(resultCode, data);
+            return;
+        }
+    }
+
+    @MainThread
+    @StyleRes
+    private int getSelectedTheme() {
+        return R.style.PurpleTheme;
+    }
+
+    @MainThread
+    @DrawableRes
+    private int getSelectedLogo() {
+        return R.drawable.logo_googleg_color_144dp;
+    }
+
+    private List<AuthUI.IdpConfig> getProviders() {
+        List<AuthUI.IdpConfig> selectedProviders = new ArrayList<>();
+        selectedProviders.add(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build());
+        selectedProviders.add(
+                new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER)
+                        .setPermissions(getFacebookPermissions())
+                        .build());
+        selectedProviders.add(
+                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER)
+                        .setPermissions(getGooglePermissions())
+                        .build());
+        return selectedProviders;
+    }
+
+    @MainThread
+    private String getSelectedTosUrl() {
+        return FIREBASE_TOS_URL;
+    }
+
+    @MainThread
+    private List<String> getGooglePermissions() {
+        List<String> result = new ArrayList<>();
+        return result;
+    }
+
+    @MainThread
+    private List<String> getFacebookPermissions() {
+        List<String> result = new ArrayList<>();
+        return result;
+    }
+
+    private void goToSignIn() {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setTheme(getSelectedTheme())
+                        .setLogo(getSelectedLogo())
+                        .setProviders(getProviders())
+                        .setTosUrl(getSelectedTosUrl())
+                        .setIsSmartLockEnabled(true)
+                        .setAllowNewEmailAccounts(true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @MainThread
+    private void handleSignInResponse(int resultCode, Intent data) {
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        // Successfully signed in
+        if (resultCode == ResultCodes.OK) {
+            Log.d(TAG, "Successful sign in");
+            startActivity(new Intent(this, ChooserActivity.class));
+            finish();
+            return;
+        } else {
+            // Sign in failed
+            Log.d(TAG, "Unsuccessful sign in");
+            if (response == null) {
+                // User pressed back button
+                //showSnackbar(R.string.sign_in_cancelled);
+                return;
+            }
+
+            if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                //showSnackbar(R.string.no_internet_connection);
+                return;
+            }
+
+            if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                //showSnackbar(R.string.unknown_error);
+                return;
+            }
+        }
+
+        //showSnackbar(R.string.unknown_sign_in_response);
+    }
+
+    public void beginFlow(View view) {
+        goToSignIn();
+    }
+
+}
