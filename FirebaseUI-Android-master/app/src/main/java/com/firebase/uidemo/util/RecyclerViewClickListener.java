32711bf05450b7b4678c763242f1c26b4c68402f
Cathy Chi <CathyChi@wl-10-190-62-193.wireless.duke.edu>
2017-04-30 13:16:26 -0400

    minor changes

--- FirebaseUI-Android-master/app/src/main/java/com/firebase/uidemo/util/RecyclerViewClickListener.java
+++ FirebaseUI-Android-master/app/src/main/java/com/firebase/uidemo/util/RecyclerViewClickListener.java
@@ -4,6 +4,9 @@ package com.firebase.uidemo.util;
  * Created by CathyChi on 3/30/17.
  */
 
+/**
+ * Used to indicate when an item is clicked in a RecyclerView
+ */
 public interface RecyclerViewClickListener {
     void recyclerViewItemClicked(int position);
 }
