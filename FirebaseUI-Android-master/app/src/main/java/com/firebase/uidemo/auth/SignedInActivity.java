9f4a4de70089f3b7049b1b73c38b049d32eaf6a9
jmb145 <jmb145@duke.edu>
2017-04-30 20:07:57 -0400

    added permissions before the camera opens

--- FirebaseUI-Android-master/app/src/main/java/com/firebase/uidemo/auth/SignedInActivity.java
+++ FirebaseUI-Android-master/app/src/main/java/com/firebase/uidemo/auth/SignedInActivity.java
@@ -14,10 +14,12 @@
 
 package com.firebase.uidemo.auth;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
@@ -26,13 +28,17 @@ import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.auth.AuthUI;
@@ -193,8 +199,41 @@ public class SignedInActivity extends AppCompatActivity {
 
     @OnClick(R.id.go_to_camera)
     public void goToCameraActivity(){
-        Intent goToCamera = new Intent(getApplicationContext(), CameraActivity.class);
-        startActivity(goToCamera);
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA);
+        if(permissionCheck != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        }
+        else {
+            Intent goToCamera = new Intent(getApplicationContext(), CameraActivity.class);
+            startActivity(goToCamera);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Intent goToCamera = new Intent(getApplicationContext(), CameraActivity.class);
+                    startActivity(goToCamera);
+
+                } else {
+
+                    Toast.makeText(getApplicationContext(), "Sorry, but you have denied camera permission.",
+                            Toast.LENGTH_LONG).show();
+
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     private void deleteAccount() {
